39a463157528845e11d3e3c9a5254bfa00768dc3
Andrew <heldiggris7@gmail.com>
2017-03-31 11:14:31 -0500

    update

---
+++ ConcertHall/ConcertHall/Source.cpp
@@ -0,0 +1,24 @@
+//Concert Hall
+//Andrew McKenzie
+//Date: 3/30/2017
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+	
+	std::string orchestra_Seats, MainFloor_Seats, Balcony_Seats;
+
+
+
+	std::cout << "Please enter how many tickets sold for Orchestra: ";
+	std::cin >> orchestra_Seats;
+
+	std::cout << "Please enter how many tickets sold for Main Floor: ";
+	std::cin >> MainFloor_Seats;
+
+	std::cout << "Please enter how many tickets sold for Balcony: ";
+	std::cin >> Balcony_Seats;
+
+}
