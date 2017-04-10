e6557f6dc54df574bb035812d2dbba00ecf094d2
Andrew <heldiggris7@gmail.com>
2017-04-10 10:55:50 -0500

    magical Pizza done

--- magicalPizza/magicalPizza/Source.cpp
+++ magicalPizza/magicalPizza/Source.cpp
@@ -7,23 +7,57 @@
 
 int main()
 {
-	int yes, SCV;
+	int yes;
 	std::string choice, type;
 
-	std::cout << "Welcome to:\nThe Prancing Unicorn Pizzaria!!";
-	std::cout << "Do you want some magical pizza? Press 1 for Yes 2 for No";
+	std::cout << "Welcome to: The Prancing Unicorn Pizzaria!!" << std::endl;
+	std::cout << "Do you want some magical pizza? Press 1 for Yes 2 for No- ";
 	std::cin >> yes;
-	if (yes==1)
-		std::cout << "What size: Imp(Small), Giant(Medium), Dragon(Large)";
+	if (yes == 1)
+	{
+		std::cout << "What size:\n Imp (Small), Giant (Medium), Dragon (Large)- ";
 		std::cin >> choice;
-		if (choice == "small" || choice == "Small" || choice == "imp" || choice == "Imp")
-			std::cout << "What type";
 
-		if (choice == "medium" || choice == "Medium" || choice == "giant" || choice == "Gaint")
+		if (choice == "small" || choice == "Small" || choice == "imp" || choice == "Imp")
+		{
+			std::cout << "What type:\nUnicorn (Sausage), Goat (Cheese), Ivy (Veggie)- ";
+			std::cin >> type;
+		}
+
+
+		if (choice == "medium" || choice == "Medium" || choice == "giant" || choice == "Giant")
+		{
+			std::cout << "What type:\n Unicorn (Sausage), Goat (Cheese), Ivy (Veggie)- ";
+			std::cin >> type;
+		}
 
 		if (choice == "large" || choice == "Large" || choice == "dragon" || choice == "Dragon")
+		{
+			std::cout << "What type:\nUnicorn (Sausage), Goat (Cheese), Ivy (Veggie)- ";
+			std::cin >> type;
+		}
+
+		std::cout << " " << std::endl;
+
+		//Price
+		if (choice == "small" || choice == "Small" || choice == "imp" || choice == "Imp")
+		{
+			std::cout << "You ordered a" << choice << " with " << type << " Toppings.\nThe cost for your pizza is 10 Gold ($5.00).\nThnk you for shopping here at The Prancing Unicorn Pizzaria!!" << std::endl;
+		}
+
+		if (choice == "medium" || choice == "Medium" || choice == "giant" || choice == "Giant")
+		{
+			std::cout << "You ordered a " << choice << " with " << type << " Toppings.\nThe cost for your pizza is 30 Gold ($15.00).\nThnk you for shopping here at The Prancing Unicorn Pizzaria!!" << std::endl;
+		}
+
+		if (choice == "large" || choice == "Large" || choice == "dragon" || choice == "Dragon")
+		{
+			std::cout << "You ordered a " << choice << " with " << type << " Toppings.\nThe cost for your pizza is 40 Gold ($20.00).\nThnk you for shopping here at The Prancing Unicorn Pizzaria!!" << std::endl;
+		}
+
+	}
 	else
-		std::cout << "Then go away" << std::endl;
+		std::cout << "Okay, Goodbye!" << std::endl;
 
 
 
