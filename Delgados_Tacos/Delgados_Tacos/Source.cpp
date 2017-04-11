6c6f228143ec180fc218eb9439858282dd1c58a9
Andrew <heldiggris7@gmail.com>
2017-04-11 11:24:32 -0500

    update if

--- Delgados_Tacos/Delgados_Tacos/Source.cpp
+++ Delgados_Tacos/Delgados_Tacos/Source.cpp
@@ -1,12 +1,14 @@
-//
+//Delgados Tacos
 //Andrew McKenzie
-//Date:
+//Date: 4/11/2017
+
 #include <iostream>
 #include <string>
 
 int main()
 {
 	std::string burrito, taco, pop, churros, tamales, tacoSalad, chipsSalsa, taquitos, chimichanga, potatoOle;
+	int order;
 
 	double burritoPrice = 1.99; //Burrito
 	double burritosOrdered;
@@ -86,102 +88,154 @@ int main()
 
 
 	std::cout << "                                           " << std::endl;
-	std::cout << "  ~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~  " << std::endl;
-	std::cout << "  |                                     |  " << std::endl;
-	std::cout << "  ~                                     ~  " << std::endl;
-	std::cout << "  |                                     |  " << std::endl;
-	std::cout << "  ~                                     ~  " << std::endl;
-	std::cout << "  |                                     |  " << std::endl;
-	std::cout << "  ~          Tacos | price | $0.99      ~  " << std::endl;
-	std::cout << "  |        Burrito | price | $1.99      |  " << std::endl;
-	std::cout << "  ~        Mt. Dew | price | $0.99      ~  " << std::endl;
-	std::cout << "  |        Churros | price | $2.00      |  " << std::endl;
-	std::cout << "  ~        Tamales | price | $1.50      ~  " << std::endl;
-	std::cout << "  |     Taco Salad | price | $2.50      |  " << std::endl;
-	std::cout << "  ~  Chips & Salsa | price | $1.00      ~  " << std::endl;
-	std::cout << "  |       Taquitos | price | $1.50      |  " << std::endl;
-	std::cout << "  ~    Chimichanga | price | $2.00      ~  " << std::endl;
-	std::cout << "  |     Potato Ole | price | $1.50      |  " << std::endl;
-	std::cout << "  ~                                     ~  " << std::endl;
-	std::cout << "  |                                     |  " << std::endl;
-	std::cout << "  ~                                     ~  " << std::endl;
-	std::cout << "  |                                     |  " << std::endl;
-	std::cout << "  ~                                     ~  " << std::endl;
-	std::cout << "  |~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|  " << std::endl;
+	std::cout << "  ~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|  " << std::endl;
+	std::cout << "  |                                        |  " << std::endl;
+	std::cout << "  ~                                        ~  " << std::endl;
+	std::cout << "  |                                        |  " << std::endl;
+	std::cout << "  ~                                        ~  " << std::endl;
+	std::cout << "  |                                        |  " << std::endl;
+	std::cout << "  ~  1         Tacos | price | $0.99       ~  " << std::endl;
+	std::cout << "  |  2       Burrito | price | $1.99       |  " << std::endl;
+	std::cout << "  ~  3       Mt. Dew | price | $0.99       ~  " << std::endl;
+	std::cout << "  |  4       Churros | price | $2.00       |  " << std::endl;
+	std::cout << "  ~  5       Tamales | price | $1.50       ~  " << std::endl;
+	std::cout << "  |  6    Taco Salad | price | $2.50       |  " << std::endl;
+	std::cout << "  ~  7 Chips & Salsa | price | $1.00       ~  " << std::endl;
+	std::cout << "  |  8      Taquitos | price | $1.50       |  " << std::endl;
+	std::cout << "  ~  9   Chimichanga | price | $2.00       ~  " << std::endl;
+	std::cout << "  |  10   Potato Ole | price | $1.50       |  " << std::endl;
+	std::cout << "  ~                                        ~  " << std::endl;
+	std::cout << "  |                                        |  " << std::endl;
+	std::cout << "  ~                                        ~  " << std::endl;
+	std::cout << "  |                                        |  " << std::endl;
+	std::cout << "  ~                                        ~  " << std::endl;
+	std::cout << "  |~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~|~  " << std::endl;
 	std::cout << "                                           " << std::endl;
 
 
 	//How many...
 
-	std::cout << "How many Tacos do you want? " << std::endl;
-	std::cin >> tacosOrdered;
+	std::cout << "Please slect the number of the order you want. ";
+	std::cin >> order;
 
-	std::cout << "How many Burritos do you want? " << std::endl;
-	std::cin >> burritosOrdered;
+	if (order == 1)
+	{
+		std::cout << "How many Tacos do you want? " << std::endl;
+		std::cin >> tacosOrdered;
+		std::cout << "Please slect the number of the order you want. Type a number <10 to stop. ";
+		std::cin >> order;
+	}
 
-	std::cout << "How many Mt. dews do you want? " << std::endl;
-	std::cin >> popOrdered;
+	if (order == 2)
+	{
+		std::cout << "How many Burritos do you want? " << std::endl;
+		std::cin >> burritosOrdered;
+		std::cout << "Please slect the number of the order you want. Type a number <10 to stop. ";
+		std::cin >> order;
+	}
 
-	std::cout << "How many churros do you want? " << std::endl;
-	std::cin >> churrosOrdered;
+	if (order == 3)
+	{
+		std::cout << "How many Mt. dews do you want? " << std::endl;
+		std::cin >> popOrdered;
+		std::cout << "Please slect the number of the order you want. Type a number <10 to stop. ";
+		std::cin >> order;
+	}
+
+	if (order == 4)
+	{
+		std::cout << "How many churros do you want? " << std::endl;
+		std::cin >> churrosOrdered;
+		std::cout << "Please slect the number of the order you want. Type a number <10 to stop. ";
+		std::cin >> order;
+	}
+
+	if (order == 5)
+	{
+		std::cout << "How many tamales do you want? " << std::endl;
+		std::cin >> tamalesOrdered;
+		std::cout << "Please slect the number of the order you want. Type a number <10 to stop. ";
+		std::cin >> order;
+	}
+
+	if (order == 6)
+	{
+		std::cout << "How many containers of taco salad do you want? " << std::endl;
+		std::cin >> tacoSaladsOrdered;
+		std::cout << "Please slect the number of the order you want. Type a number <10 to stop. ";
+		std::cin >> order;
+	}
+
+	if (order == 7)
+	{
+		std::cout << "How many bowls of chips & salsa do you want? " << std::endl;
+		std::cin >> chipsSalsaOrdered;
+		std::cout << "Please slect the number of the order you want. Type a number <10 to stop. ";
+		std::cin >> order;
+	}
+
+	if (order == 8)
+	{
+		std::cout << "How many taquitos do you want? " << std::endl;
+		std::cin >> taquitosOrdered;
+		std::cout << "Please slect the number of the order you want. Type a number <10 to stop. ";
+		std::cin >> order;
+	}
+
+	if (order == 9)
+	{
+		std::cout << "How many chimichanga do you want? " << std::endl;
+		std::cin >> chimichangasOrdered;
+		std::cout << "Please slect the number of the order you want. Type a number <10 to stop. ";
+		std::cin >> order;
+	}
+
+	if (order == 10)
+	{
+		std::cout << "How many containers of potato oles do you want? " << std::endl;
+		std::cin >> potatoOlesOrdered;
+		std::cout << "Please slect the number of the order you want. Type a number <10 to stop. ";
+		std::cin >> order;
+	}
+	else
+	{
 
 
-	std::cout << "How many tamales do you want? " << std::endl;
-	std::cin >> tamalesOrdered;
+
+		std::cout << " " << std::endl;
 
 
-	std::cout << "How many containers of taco salad do you want? " << std::endl;
-	std::cin >> tacoSaladsOrdered;
+		//Math
 
-
-	std::cout << "How many bowls of chips & salsa do you want? " << std::endl;
-	std::cin >> chipsSalsaOrdered;
-
-
-	std::cout << "How many taquitos do you want? " << std::endl;
-	std::cin >> taquitosOrdered;
-
-
-	std::cout << "How many chimichanga do you want? " << std::endl;
-	std::cin >> chimichangasOrdered;
-
-
-	std::cout << "How many containers of potato oles do you want? " << std::endl;
-	std::cin >> potatoOlesOrdered;
-
-	std::cout << "                                                               " << std::endl;
-
-
-	//Math
-
-	totalPrice = (burritosOrdered * burritoPrice) + (tacoPrice * tacosOrdered) + (popOrdered * popPrice)
-		+ (churrosOrdered * churrosPrice) + (tamalesOrdered * tamalesPrice) + (tacoSaladsOrdered * tacoSaladPrice)
-		+ (chipsSalsaOrdered * chipsSalsaPrice) + (taquitosOrdered * taquitosPrice) + (chimichangasOrdered * chimichangaPrice)
-		+ (potatoOlesOrdered * potatoOlePrice);
+		totalPrice = (burritosOrdered * burritoPrice) + (tacoPrice * tacosOrdered) + (popOrdered * popPrice)
+			+ (churrosOrdered * churrosPrice) + (tamalesOrdered * tamalesPrice) + (tacoSaladsOrdered * tacoSaladPrice)
+			+ (chipsSalsaOrdered * chipsSalsaPrice) + (taquitosOrdered * taquitosPrice) + (chimichangasOrdered * chimichangaPrice)
+			+ (potatoOlesOrdered * potatoOlePrice);
 
 
 
-	totalPriceWithTax = totalPrice * taxRate;
+		totalPriceWithTax = totalPrice * taxRate;
 
-	std::cout << "                                                               " << std::endl;
+		std::cout << " " << std::endl;
 
-	//Total
+		//Total
 
-	std::cout << "You ordered " << tacosOrdered << " tacos, " << burritosOrdered << " burritos, " << popOrdered << " Mt. Dews, " << churrosOrdered << " churros, \n" << tamalesOrdered << " tamales, "
-		<< tacoSaladsOrdered << " containers of taco salad, " << chipsSalsaOrdered << " bowls of chips & salsa, \n" << taquitosOrdered << " taquitos, "
-		<< chimichangasOrdered << " chimichangas, and " << potatoOlesOrdered << " containers of potato oles, for a total of \n$"
-		<< totalPrice << std::endl;
+		std::cout << "You ordered " << tacosOrdered << " tacos, " << burritosOrdered << " burritos, " << popOrdered << " Mt. Dews, " << churrosOrdered << " churros, \n" << tamalesOrdered << " tamales, "
+			<< tacoSaladsOrdered << " containers of taco salad, " << chipsSalsaOrdered << " bowls of chips & salsa, \n" << taquitosOrdered << " taquitos, "
+			<< chimichangasOrdered << " chimichangas, and " << potatoOlesOrdered << " containers of potato oles, for a total of \n$"
+			<< totalPrice << std::endl;
 
-	std::cout << "                                                               " << std::endl;
+		std::cout << " " << std::endl;
 
-	//Total with tax
+		//Total with tax
 
-	std::cout << "Your total for all items entered with tax is $" << totalPriceWithTax << std::endl;
-
-
+		std::cout << "Your total for all items entered with tax is $" << totalPriceWithTax << std::endl;
 
 
+	}
 
 
 
+	system("pause");
+	return 0;
 }
