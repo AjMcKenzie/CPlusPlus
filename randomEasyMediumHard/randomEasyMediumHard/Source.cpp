25b7267e21ef5915c0159ea20752afffe39caf11
Andrew <heldiggris7@gmail.com>
2017-04-19 10:31:07 -0500

    easy-hardRandom

--- randomEasyMediumHard/randomEasyMediumHard/Source.cpp
+++ randomEasyMediumHard/randomEasyMediumHard/Source.cpp
@@ -21,7 +21,12 @@ int main()
 
 	if (difficulty == "Easy")
 	{
-
+		for (int i = 0; i < 6; i++)
+		{
+			std::cout << "Choose a number?" << std::endl;
+			std::cin >> number;
+			std::cout << i <<" ";
+		}
 	}
 
 	if (difficulty == "Medium")
