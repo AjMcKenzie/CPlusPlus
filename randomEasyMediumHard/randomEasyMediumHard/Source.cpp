66929a41b0458b0ae6865b161839f07b7682e5f5
Andrew <heldiggris7@gmail.com>
2017-04-21 11:15:13 -0500

    Easy

--- randomEasyMediumHard/randomEasyMediumHard/Source.cpp
+++ randomEasyMediumHard/randomEasyMediumHard/Source.cpp
@@ -11,32 +11,95 @@ int main()
 {
 	int number;
 	std::string difficulty;
+	//std::string contString;
 
-	srand(static_cast<unsigned int>(time(0))); // Random number generator
+	/*srand(static_cast<unsigned int>(time(0))); // Random number generator
 	int randomNumber = rand(); // Generate random number
-	int die = (randomNumber % 10) + 1; // Get a number between 1 and 10
+	int die = (randomNumber % 10) + 1; // Get a number between 1 and 10*/
 
 	std::cout << "What difficulty do you want? Easy, Medium, Hard." << std::endl;
 	std::cin >> difficulty;
 
 	if (difficulty == "Easy")
 	{
-		for (int i = 0; i < 6; i++)
+		for (int i = 0; i < 5;)
 		{
-			std::cout << "Choose a number?" << std::endl;
-			std::cin >> number;
-			std::cout << i <<" ";
+			srand(static_cast<unsigned int>(time(0))); // Random number generator
+			int randomNumber = rand(); // Generate random number
+			int die = (randomNumber % 10) + 1; // Get a number between 1 and 10
+			do{
+
+				i++;
+
+				std::cout << "Choose a number between 1 and 10." << std::endl;
+				std::cin >> number;
+			
+					if (number == die)
+					{
+						std::cout << " " << std::endl;
+						std::cout << "You got it right!" << std::endl;
+					//	std::cout << "Continue or Exit.... " << std::endl;
+					//	std::cin >> contStop;
+					//	if (contStop == "Continue")
+					//	{
+					//		difficulty == "Easy";
+					//	}
+					//	else if (contStop == "Exit")
+					//	{
+					//		std::cout << "What difficulty do you want? Easy, Medium, Hard." << std::endl;
+					//		std::cin >> difficulty;
+					//	}
+					}
+					else if (number > die)
+					{
+						std::cout << " " << std::endl;
+						std::cout << "You chose too high!" << std::endl;
+					}
+					else
+					{
+						std::cout << " " << std::endl;
+						std::cout << "You chose too low!" << std::endl;
+					}
+					std::cout << "You have used " << i << " out of 5 chances" << std::endl;
+					std::cout << "The number was " << die << std::endl;
+					std::cout << " " << std::endl;
+
+			} while (i != 5 || number == die);
+			
 		}
 	}
 
 	if (difficulty == "Medium")
 	{
+		for (int j = 0; j < 5;)
+		{
+			srand(static_cast<unsigned int>(time(0))); // Random number generator
+			int randomNumber = rand(); // Generate random number
+			int die = (randomNumber % 100) + 1; // Get a number between 1 and 100
+			j++;
 
+			std::cout << "Choose a number between 1 and 100." << std::endl;
+			std::cin >> number;
+			
+
+				std::cout << "You have used " << j << " out of 5 chances" << std::endl;
+			}
+		
 	}
 	
 	if (difficulty == "Hard")
 	{
+		for (int l = 0; l < 5;)
+		{
+			srand(static_cast<unsigned int>(time(0))); // Random number generator
+			int randomNumber = rand(); // Generate random number
+			int die = (randomNumber % 200) + 1; // Get a number between 1 and 200
+			l++;
 
+			std::cout << "Choose a number between 1 and 200." << std::endl;
+			std::cin >> number;
+			std::cout << "You have used " << l << " out of 5 chances" << std::endl;
+		}
 	}
 
 	system("pause");
