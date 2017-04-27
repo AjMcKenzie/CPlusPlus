d960145286a4b22d5c98a1d1fed88d1a70276bbe
Andrew <heldiggris7@gmail.com>
2017-04-27 11:30:43 -0500

    trying+

--- MadLibsFunction/MadLibsFunction/Source.cpp
+++ MadLibsFunction/MadLibsFunction/Source.cpp
@@ -6,7 +6,6 @@
 
 int Color()
 {
-	
 	std::string color;
 	std::cout << "Type a color: ";
 	std::cin >> color;
@@ -22,44 +21,74 @@ int WordEst()
 	return;
 }
 
-int main()
+int BodyParts()
 {
-
-	int a;
-	int b;
-	int c;
-
-	std::string color, wordEst, body_parts, animal, noun, plural_noun, Co, Est;
-
-	Co = Color();
-
-	Est = WordEst();
-	
-
+	std::string body_parts;
 	std::cout << "Type a plural body part: ";
 	std::cin >> body_parts;
+	return;
+}
 
+int Animal()
+{
+	std::string animal;
 	std::cout << "Type an animal: ";
 	std::cin >> animal;
+	return;
+}
 
+int Noun()
+{
+	std::string noun;
 	std::cout << "Type a noun: ";
 	std::cin >> noun;
+	return;
+}
 
+int PluralNoun()
+{
+	std::string plural_noun;
 	std::cout << "Type a plural noun: ";
 	std::cin >> plural_noun;
+	return;
+}
 
+int A(int a)
+{
+	int a;
 	std::cout << "Type a number: ";
 	std::cin >> a;
+	return a;
+}
 
+int B(int b)
+{
+	int b;
 	std::cout << "Type another number that is less then the first: ";
 	std::cin >> b;
+	return b;
+}
 
+int C(int c, int a, int b)
+{
 	c = a - b;
+}
+
+int main(int a, int b, int c)
+{
+
+
+	std::string color, wordEst, body_parts, animal, noun, plural_noun;
+
+	
+	
+
+	
 	std::cout << "                                                            " << std::endl;
 	std::cout << "                                                            " << std::endl;
 
-	std::cout << "The " << Color() << " Dragon is the " << WordEst() << " Dragon of all. \nIt has " << c << " " << body_parts << ", and a " << animal << " shaped like a " << noun << "." << std::endl;
-	std::cout << "It loves to eat " << plural_noun << ", although it will feast on nearly anything." << std::endl;
+	std::cout << "The " << Color() << " Dragon is the " << WordEst() << " Dragon of all. \nIt has " << C(c, a, b) << " " << BodyParts() << ", and a " << Animal() << " shaped like a " << Noun() << "." << std::endl;
+	std::cout << "It loves to eat " << PluralNoun() << ", although it will feast on nearly anything." << std::endl;
 
 	system("pause");
 	return 0;
--- Right_Triangle/Right_Triangle/Source.cpp
+++ Right_Triangle/Right_Triangle/Source.cpp
@@ -6,6 +6,50 @@
 #include <string>
 #include <math.h>
 
+
+int displayAside()
+{
+	double userEntryC, userEntryB, answerA;
+	std::cout << "Enter you number for side B >>> ";
+	std::cin >> userEntryB;
+
+	std::cout << "Enter you number for side C >>> ";
+	std::cin >> userEntryC;
+
+	answerA = sqrt(userEntryC * userEntryC - userEntryB * userEntryB);
+	std::cout << "The answer for side A is " << answerA << "\n";
+	return answerA;
+
+}
+
+int displayBside()
+{
+	double userEntryA, userEntryC, answerB;
+	std::cout << "Enter you number for side A >>> ";
+	std::cin >> userEntryA;
+
+	std::cout << "Enter you number for side C >>> ";
+	std::cin >> userEntryC;
+
+	answerB = sqrt(userEntryA * userEntryA - userEntryC * userEntryC);
+	std::cout << "The answer for side B is " << answerB << "\n";
+	return answerB;
+}
+
+int displayCside()
+{
+	double userEntryA, userEntryB, answerC;
+	std::cout << "Enter you number for side A >>> ";
+	std::cin >> userEntryA;
+
+	std::cout << "Enter you number for side B >>> ";
+	std::cin >> userEntryB;
+
+	answerC = sqrt(userEntryA * userEntryA - userEntryB * userEntryB);
+	std::cout << "The answer for side C is " << answerC << "\n";
+	return answerC;
+}
+
 int main()
 {
 	std::string whichSide, side, A, a, B, b, C, c;
@@ -19,56 +63,23 @@ int main()
 
 		if (side == A || side == a)
 		{
-			double userEntryC, userEntryB, answerA;
-			std::cout << "Enter you number for side B >>> ";
-			std::cin >> userEntryB;
-
-			std::cout << "Enter you number for side C >>> ";
-			std::cin >> userEntryC;
-
-			answerA = sqrt(userEntryC * userEntryC - userEntryB * userEntryB);
-			std::cout << "The answer for side A is " << answerA << "\n";
-
-			std::cout << "\nWould you like to solve for another triangle?";
-			std::cout << "\nEnter 1 for yes, or any other number for no >>>> ";
-			std::cin >> responds;
+			displayAside();
 		}
 
 		if (side == B || side == b)
 		{
-			double userEntryA, userEntryC, answerB;
-			std::cout << "Enter you number for side A >>> ";
-			std::cin >> userEntryA;
-
-			std::cout << "Enter you number for side C >>> ";
-			std::cin >> userEntryC;
-
-			answerB = sqrt(userEntryA * userEntryA - userEntryC * userEntryC);
-			std::cout << "The answer for side B is " << answerB << "\n";
-
-			std::cout << "\nWould you like to solve for another triangle?";
-			std::cout << "\nEnter 1 for yes, or any other number for no >>>> ";
-			std::cin >> responds;
+			displayBside();
 		}
 		if (side == C || side == c)
 		{
-			double userEntryA, userEntryB, answerC;
-			std::cout << "Enter you number for side A >>> ";
-			std::cin >> userEntryA;
-
-			std::cout << "Enter you number for side B >>> ";
-			std::cin >> userEntryB;
-
-			answerC = sqrt(userEntryA * userEntryA - userEntryB * userEntryB);
-			std::cout << "The answer for side C is " << answerC << "\n";
-
-			std::cout << "\nWould you like to solve for another triangle?";
-			std::cout << "\nEnter 1 for yes, or any other number for no >>>> ";
-			std::cin >> responds;
+			displayCside();
 		}
-
+		std::cout << "\nWould you like to solve for another triangle?";
+		std::cout << "\nEnter 1 for yes, or any other number for no >>>> ";
+		std::cin >> responds;
 	}
 
 	system("pause");
 	return 0;
 }
+
