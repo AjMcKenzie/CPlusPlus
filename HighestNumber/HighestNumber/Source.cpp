57067139c264f2a3772da347823545ffb083232f
Andrew <heldiggris7@gmail.com>
2017-05-04 11:29:06 -0500

    highestNumber

---
+++ HighestNumber/HighestNumber/Source.cpp
@@ -0,0 +1,18 @@
+//Print Highest Number
+//Name: Andrew McKenzie
+//Date: 5/4/2017
+
+#include <iostream>
+
+int numbers[6] = { 23, 6, 47, 35, 2, 14 }; //The array
+int n, result = 0;
+
+int main()
+{
+	
+
+	std::cout << "The highest number is: " /*<< highest */<< std::endl;;
+
+	system("pause");
+	return 0;
+}
