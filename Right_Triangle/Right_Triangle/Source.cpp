004627ce6506091242d14aceaae28e257cd6e8df
Andrew <heldiggris7@gmail.com>
2017-05-26 09:59:25 -0500

    Right_Triangle_Done

--- Right_Triangle/Right_Triangle/Source.cpp
+++ Right_Triangle/Right_Triangle/Source.cpp
@@ -6,6 +6,47 @@
 #include <string>
 #include <math.h>
 
+int displayAside()
+{
+	double userEntryC, userEntryB, answerA;
+	std::cout << "Enter you number for side B >>> ";
+	std::cin >> userEntryB;
+
+	std::cout << "Enter you number for side C >>> ";
+	std::cin >> userEntryC;
+
+	answerA = sqrt(userEntryC * userEntryC - userEntryB * userEntryB);
+	std::cout << "The answer for side A is " << answerA << "\n";
+	return 0;
+}
+
+int displayBside()
+{
+	double userEntryA, userEntryC, answerB;
+	std::cout << "Enter you number for side A >>> ";
+	std::cin >> userEntryA;
+
+	std::cout << "Enter you number for side C >>> ";
+	std::cin >> userEntryC;
+
+	answerB = sqrt(userEntryA * userEntryA - userEntryC * userEntryC);
+	std::cout << "The answer for side B is " << answerB << "\n";
+	return 0;
+}
+
+int displayCside()
+{
+	double userEntryA, userEntryB, answerC;
+	std::cout << "Enter you number for side A >>> ";
+	std::cin >> userEntryA;
+
+	std::cout << "Enter you number for side B >>> ";
+	std::cin >> userEntryB;
+
+	answerC = sqrt(userEntryA * userEntryA - userEntryB * userEntryB);
+	std::cout << "The answer for side C is " << answerC << "\n";
+	return 0;
+}
 
 
 int main()
@@ -20,43 +61,16 @@ int main()
 
 			if (side == "A" || side == "a")
 			{
-				double userEntryC, userEntryB, answerA;
-				std::cout << "Enter you number for side B >>> ";
-				std::cin >> userEntryB;
-
-				std::cout << "Enter you number for side C >>> ";
-				std::cin >> userEntryC;
-
-				answerA = sqrt(userEntryC * userEntryC - userEntryB * userEntryB);
-				std::cout << "The answer for side A is " << answerA << "\n";
-				//displayAside();
+				displayAside();
 			}
 
 			if (side == "B" || side == "b")
 			{
-				double userEntryA, userEntryC, answerB;
-				std::cout << "Enter you number for side A >>> ";
-				std::cin >> userEntryA;
-
-				std::cout << "Enter you number for side C >>> ";
-				std::cin >> userEntryC;
-
-				answerB = sqrt(userEntryA * userEntryA - userEntryC * userEntryC);
-				std::cout << "The answer for side B is " << answerB << "\n";
-				//displayBside();
+				displayBside();
 			}
 			if (side == "C" || side == "c")
 			{
-				double userEntryA, userEntryB, answerC;
-				std::cout << "Enter you number for side A >>> ";
-				std::cin >> userEntryA;
-
-				std::cout << "Enter you number for side B >>> ";
-				std::cin >> userEntryB;
-
-				answerC = sqrt(userEntryA * userEntryA - userEntryB * userEntryB);
-				std::cout << "The answer for side C is " << answerC << "\n";
-				//displayCside();
+				displayCside();
 			}
 			std::cout << "\nWould you like to solve for another triangle?";
 			std::cout << "\nEnter 1 for yes, or any other number for no >>>> ";
@@ -66,47 +80,3 @@ int main()
 	system("pause");
 	return 0;
 }
-
-
-/*int displayAside()
-{
-	double userEntryC, userEntryB, answerA;
-	std::cout << "Enter you number for side B >>> ";
-	std::cin >> userEntryB;
-
-	std::cout << "Enter you number for side C >>> ";
-	std::cin >> userEntryC;
-
-	answerA = sqrt(userEntryC * userEntryC - userEntryB * userEntryB);
-	std::cout << "The answer for side A is " << answerA << "\n";
-	return answerA;
-}
-
-int displayBside()
-{
-	double userEntryA, userEntryC, answerB;
-	std::cout << "Enter you number for side A >>> ";
-	std::cin >> userEntryA;
-
-	std::cout << "Enter you number for side C >>> ";
-	std::cin >> userEntryC;
-
-	answerB = sqrt(userEntryA * userEntryA - userEntryC * userEntryC);
-	std::cout << "The answer for side B is " << answerB << "\n";
-	return answerB;
-}
-
-int displayCside()
-{
-	double userEntryA, userEntryB, answerC;
-	std::cout << "Enter you number for side A >>> ";
-	std::cin >> userEntryA;
-
-	std::cout << "Enter you number for side B >>> ";
-	std::cin >> userEntryB;
-
-	answerC = sqrt(userEntryA * userEntryA - userEntryB * userEntryB);
-	std::cout << "The answer for side C is " << answerC << "\n";
-	return answerC;
-}*/
-
